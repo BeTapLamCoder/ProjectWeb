7ee6da8797e862ae8a4bf190ba745be3a0bb05eb
Gia Huy <huyhaibabon861@gmail.com>
2025-05-29 21:56:51 +0700

    feat: build user class

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const healthRoute = require('./routes/health.route');
+const userRoute = require('./routes/user.route');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/health', healthRoute);
+app.use('/users', userRoute);
 
 const PORT = process.env.PORT || 8080;
 
