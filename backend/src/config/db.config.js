f7d961dda78865b816f2cee27aaf587a20e0ab96
giahuytr <huyhaibabon861@gmail.com>
2025-05-30 12:08:47 +0700

    Fix/update database config (#73)

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
-  user: 'jeanshopdev',
-  host: 'localhost',
-  database: 'jeanshop',
-  password: 'jeanshopdev',
-  port: 5433,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = {
